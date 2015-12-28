155c49190ea05d6ce2209d98af0dc0685a23ee7a
Sébastien Forestier <sebastien.forestier@inria.fr>
2015-12-28 18:06:05 +0100

    player1

--- src/bateau.py
+++
@@ -1,6 +0,0 @@
-class Bateau(Marine):
-        def __init__(self):
-            
-        def affiche_heure(self):
-            print str(self.heure) + ":" + str(self.minute) \
-                 + ":" + str(self.seconde)
--- src/explorateur.py
+++ src/explorateur.py
@@ -1,6 +1,11 @@
-class Explorateur(Bateau):
-        def __init__(self):
-            self.vision = 10
-            self.vitesse = 1.25
-            self.discretion = 0
-            self.puissance = 0
+
+import pygame
+from marine import Marine
+
+class Explorateur(Marine):
+    def __init__(self, w, h, position):
+        Marine.__init__(self, w, h, position)
+        self.vision = 10
+        self.vitesse = 1.25
+        self.discretion = 0
+        self.puissance = 0
